0e7bbdb4c3740ff5b1d135b26989b137ccc4b069
Airbus6804 <92552578+Airbus6804@users.noreply.github.com>
2023-02-08 15:52:32 +0100

    Svolgimento esercizio Object Methods

---
+++ exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+Object.keys(person).forEach(p => {
+  console.log(`${p}: ${person[p]}`)
+})
+
+// Print values of person using Object.keys
